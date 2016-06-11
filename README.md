dc7726eaf135b563cda27f546170855c040d0f56
tandav <alx1dr@gmail.com>
2016-06-11 07:12:25 +0300

    Backup

--- README.md
+++ README.md
@@ -1,10 +1,7 @@
-# Электронный стетоскоп [Beta]
+# Электронный стетоскоп 
 My coursework at Voronezh State University
 
 ##todo
 
-1. Купить микрофон
-2. Богатиков
-3. add log scale
-4. Try to find best performance parameters
-5. Отчет написать по стетоскопу. (Воды налить ебаной)
+1. Record
+2. Свертка
--- matplotlib_fft.py
+++ matplotlib_fft.py
@@ -1,15 +1,20 @@
+import matplotlib
+matplotlib.use('TKAgg')
 from matplotlib.animation import FuncAnimation
-from audiolazy import lazy_lpc as lpc
+from matplotlib.widgets import Button
+
+
+# from audiolazy import lazy_lpc as lpc
 import matplotlib.pyplot as plt
 import numpy as np
 import pyaudio
 import sys
+import wave
 
 #############################
-# GUI parameters
+# GUI Prameters
 #############################
-headless = False
-timeDomain = False
+timeDomain = True
 freqDomain = True
 lpcOverlay = False
 
@@ -27,7 +32,7 @@ RATE = 44100
 # Spectral parameters
 #############################
 ORDER = 12
-NFFT = CHUNK/2
+NFFT = CHUNK/2/2
 # correlation: 12*4*1024 = 49152
 # matrix solution: 12*12 = 144
 # matrix solution cov: 12*12*12 = 1728
@@ -86,87 +91,82 @@ stream = p.open(format=FORMAT,
 # Fetch incoming data
 ##########################
 errorCount = [0]
-if not headless:
-    oldy = range(2*CHUNK)
+oldy = range(2*CHUNK)
 
-    ######################################################
-    # Create new Figure and an Axes to occupy the figure.
-    ######################################################
-    fig = plt.figure(facecolor='white')  # optional arg: figsize=(x, y)
-    nAx = sum([1 for xx in [timeDomain, freqDomain] if xx])
+######################################################
+# Create new Figure and an Axes to occupy the figure.
+######################################################
+fig = plt.figure(facecolor='white')  # optional arg: figsize=(x, y)
+nAx = sum([1 for xx in [timeDomain, freqDomain] if xx])
 
-    if timeDomain:
-        axTime = fig.add_axes([.1, .5*nAx - .5 + .1/nAx, .8, .4*(3-nAx)])
-        axTime.set_axis_bgcolor('#c6dbef')
-        plt.grid()
-        lineTime, = axTime.plot(range(2*CHUNK), range(2*CHUNK), c='#08519c')
-        plt.ylim([-4000, 4000])
-        plt.xlim([0, 2*CHUNK])
-        plt.title('Real Time Audio (milliseconds)')
-        axTime.set_xticks(np.linspace(0, 2*CHUNK, 5))
-        labels = ['%.1f' % (xx) for xx in np.linspace(0, 1000*2*CHUNK/RATE, 5)]
-        axTime.set_xticklabels(labels, rotation=0, verticalalignment='top')
-        plt.ylabel('Amplitude')
+if timeDomain:
+    axTime = fig.add_axes([.1, .5*nAx - .5 + .1/nAx, .8, .4*(3-nAx)])
+    axTime.set_axis_bgcolor('#c6dbef')
+    plt.grid()
+    lineTime, = axTime.plot(range(2*CHUNK), range(2*CHUNK), c='#08519c')
+    plt.ylim([-4000, 4000])
+    plt.xlim([0, 2*CHUNK])
+    plt.title('Real Time Audio (milliseconds)')
+    axTime.set_xticks(np.linspace(0, 2*CHUNK, 5))
+    labels = ['%.1f' % (xx) for xx in np.linspace(0, 1000*2*CHUNK/RATE, 5)]
+    axTime.set_xticklabels(labels, rotation=0, verticalalignment='top')
+    plt.ylabel('Amplitude')
 
-    if freqDomain:
-        axFreq = fig.add_axes([.1, .1/nAx, .8, 0.4*(3-nAx)])
-        axFreq.set_axis_bgcolor('#c6dbef')
-        plt.grid()
-        axFreq.set_xscale('log')
+if freqDomain:
+    axFreq = fig.add_axes([.1, .1/nAx, .8, 0.4*(3-nAx)])
+    axFreq.set_axis_bgcolor('#c6dbef')
+    plt.grid()
+    lineFreq, = axFreq.plot(range(NFFT/2), [0]*(NFFT/2),
+                            c='#6baed6', label='FFT')
+    if lpcOverlay:
+        lineLPC, = axFreq.plot(range(NFFT/2), [0]*(NFFT/2),
+                               '--', c='#08519c', label='LPC Envelope')
+    plt.ylim([-50, 300])
+    plt.xlim([0, NFFT/2])
+    plt.title('Real Time Audio (Hz)')
+    # plt.xscale('log')
 
-        lineFreq, = axFreq.plot(range(NFFT/2), [0]*(NFFT/2),
-                                c='#6baed6', label='FFT')
-        if lpcOverlay:
-            lineLPC, = axFreq.plot(range(NFFT/2), [0]*(NFFT/2),
-                                   '--', c='#08519c', label='LPC Envelope')
-        plt.ylim([-50, 300])
-        plt.xlim([0, NFFT/2])
-        plt.title('Real Time Audio (Hz)')
-        plt.legend()
-        axFreq.set_xticks(np.linspace(0, NFFT/2, 5))
-        labels = ['%.1f' % (xx) for xx in np.linspace(0, RATE/2, 5)]
-        axFreq.set_xticklabels(labels, rotation=0, verticalalignment='top')
-        plt.ylabel('Amplitude [dB]')
+    plt.legend()
+    axFreq.set_xticks(np.linspace(0, NFFT/2, 5))
+    labels = ['%.1f' % (xx) for xx in np.linspace(0, RATE/2, 5)]
+    axFreq.set_xticklabels(labels, rotation=0, verticalalignment='top')
+    plt.ylabel('Amplitude [dB]')
+    # axFreq.set_xscale('log')
+
 
 
     ######################################################
     # Define function to update figure for each iteration.
     ######################################################
-    def update(frame_number):
-        global oldy
-        objects_to_return = []
-        try:
-            incoming = np.fromstring(stream.read(CHUNK), 'Int16')
-            if timeDomain:
-                newy = oldy[CHUNK:]
-                newy.extend(incoming)
-                lineTime.set_ydata(newy)
-                objects_to_return.append(lineTime)
-                oldy = newy
-            if freqDomain:
-                windowed = incoming*WINDOW
-                S = spectral_estimate(windowed)
-                lineFreq.set_ydata(S)
-                if lpcOverlay:
-                    S_lpc = lpc_spectrum(windowed)
-                    lineLPC.set_ydata(S_lpc)
-                objects_to_return.append(lineFreq)
-                # objects_to_return.append(lineLPC)
-        except IOError as e:
-            print str(e),
-            errorCount[0] += 1
-            if errorCount[0] > 5:
-                print "This is happening often.",
-                print " (Try increasing size of \'CHUNK\'.)"
-            else:
-                print
-        return objects_to_return
-
-    animation = FuncAnimation(fig, update, interval=10)
-    plt.show()
-
-else:
-    while True:
+def update(frame_number):
+    global oldy
+    objects_to_return = []
+    try:
         incoming = np.fromstring(stream.read(CHUNK), 'Int16')
-        S = spectral_estimate(windowed)
-        #  Do some processing stuff here!
+        if timeDomain:
+            newy = oldy[CHUNK:]
+            newy.extend(incoming)
+            lineTime.set_ydata(newy)
+            objects_to_return.append(lineTime)
+            oldy = newy
+        if freqDomain:
+            windowed = incoming*WINDOW
+            S = spectral_estimate(windowed)
+            lineFreq.set_ydata(S)
+            if lpcOverlay:
+                S_lpc = lpc_spectrum(windowed)
+                lineLPC.set_ydata(S_lpc)
+            objects_to_return.append(lineFreq)
+            # objects_to_return.append(lineLPC)
+    except IOError as e:
+        print str(e),
+        errorCount[0] += 1
+        if errorCount[0] > 5:
+            print "This is happening often.",
+            print " (Try increasing size of \'CHUNK\'.)"
+        else:
+            print
+    return objects_to_return
+
+animation = FuncAnimation(fig, update, interval=10)
+plt.show()
