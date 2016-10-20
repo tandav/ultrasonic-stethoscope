7fe87baa5a0f0ad54544483e9d09161afa3d76a3
Alexander Rodionov <alx1dr@gmail.com>
2016-10-20 10:08:55 +0300

    added working chart c#

---
+++ plot-test2/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace plot_test2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        double r = 0.1;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            chart1.Series["Series1"].Points.Clear();
+            for (int i = 0; i < 500; i++)
+            {
+                chart1.Series["Series1"].Points.AddXY(i, r*Math.Sin(i));
+                r += 0.1;
+            }
+        }
+    }
+}
