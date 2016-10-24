394404fc4fe958e82206d092a348f57e499550ce
tnd <tnd@TND-PC>
2016-10-24 21:44:42 +0300

    both charts 2/2

--- kernelchip-sensors-mic/kernelchip-sensors-mic/Form1.Designer.cs
+++ kernelchip-sensors-mic/kernelchip-sensors-mic/Form1.Designer.cs
@@ -29,28 +29,41 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
             this.SuspendLayout();
             // 
             // chart1
             // 
-            chartArea2.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.chart1.Legends.Add(legend2);
+            this.chart1.BorderSkin.BackImageWrapMode = System.Windows.Forms.DataVisualization.Charting.ChartImageWrapMode.TileFlipY;
+            chartArea1.AxisX.MajorGrid.LineColor = System.Drawing.Color.WhiteSmoke;
+            chartArea1.AxisY.MajorGrid.LineColor = System.Drawing.Color.WhiteSmoke;
+            chartArea1.BackSecondaryColor = System.Drawing.Color.MediumSpringGreen;
+            chartArea1.BorderWidth = 10;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.DockedToChartArea = "ChartArea1";
+            legend1.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Left;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
             this.chart1.Location = new System.Drawing.Point(12, 12);
             this.chart1.Name = "chart1";
-            series2.ChartArea = "ChartArea1";
-            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            this.chart1.Series.Add(series2);
-            this.chart1.Size = new System.Drawing.Size(811, 323);
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series1.Legend = "Legend1";
+            series1.Name = "Mic";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(1000, 220);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
             // 
@@ -59,17 +72,51 @@
             this.timer1.Interval = 10;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // chart2
+            // 
+            chartArea2.AxisX.MajorGrid.LineColor = System.Drawing.Color.WhiteSmoke;
+            chartArea2.AxisY.MajorGrid.LineColor = System.Drawing.Color.WhiteSmoke;
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.DockedToChartArea = "ChartArea1";
+            legend2.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Left;
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(12, 248);
+            this.chart2.Name = "chart2";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series2.Legend = "Legend1";
+            series2.Name = "Temperature Sensors";
+            this.chart2.Series.Add(series2);
+            this.chart2.Size = new System.Drawing.Size(1000, 220);
+            this.chart2.TabIndex = 1;
+            this.chart2.Text = "chart2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 477);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Voronezh State University";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(835, 420);
+            this.ClientSize = new System.Drawing.Size(1026, 499);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart2);
             this.Controls.Add(this.chart1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -77,5 +124,7 @@
 
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.Label label1;
     }
 }
--- kernelchip-sensors-mic/kernelchip-sensors-mic/Form1.cs
+++ kernelchip-sensors-mic/kernelchip-sensors-mic/Form1.cs
@@ -9,56 +9,122 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Diagnostics;
 using NAudio.Wave;
-using NAudio;
+using System.IO.Ports;
 
 namespace kernelchip_sensors_mic
 {
     public partial class Form1 : Form
     {
-        int n = 4000; // number of x-axis pints
+        //mic
+        int n_mic = 2000; //number of x-axis points
         //Stopwatch time = new Stopwatch();
         WaveIn wi;
-        Queue<double> myQ;
+        Queue<double> mic_Q;
+
+        //sensors
+        SerialPort serialPort1;
+        int n_sensors = 2000; // number of x-axis points
+        //Stopwatch time = new Stopwatch();
+        Queue<double> sensor1_Q;
+        Queue<double> sensor2_Q;
+
         public Form1()
         {
             InitializeComponent();
-            myQ = new Queue<double>(Enumerable.Repeat(0.0, n).ToList()); // fill myQ w/ zeros
-            chart1.ChartAreas[0].AxisY.Minimum = -10000;
-            chart1.ChartAreas[0].AxisY.Maximum = 10000;
         }
 
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            //mic
+            mic_Q = new Queue<double>(Enumerable.Repeat(0.0, n_mic).ToList()); // fill mic_Q w/ zeros
+            chart1.ChartAreas[0].AxisY.Minimum = -10000;
+            chart1.ChartAreas[0].AxisY.Maximum = 10000;
             wi = new WaveIn();
             wi.StartRecording();
             wi.WaveFormat = new WaveFormat(4, 16, 1); // (44100, 16, 1);
             wi.DataAvailable += new EventHandler<WaveInEventArgs>(wi_DataAvailable);
             timer1.Enabled = true;
             //time.Start();
+
+            //sensors
+            sensor1_Q = new Queue<double>(Enumerable.Repeat(0.0, n_sensors).ToList()); // fill w/ zeros
+            serialPort1 = new SerialPort();
+            serialPort1.ReadTimeout = 100; // default=50
+            serialPort1.WriteTimeout = 100; // default=50
+            if (SearchKeUSB()) timer1.Enabled = true;
+            else label1.Text = "Error: Can't connect to Kernel Chip";
         }
 
+        void draw_charts()
+        {
+            //mic
+            try { chart1.Series["Mic"].Points.DataBindY(mic_Q); }
+            catch { Console.WriteLine("No bytes recorded"); }
+            //sensors
+            chart2.Series["Temperature Sensors"].Points.DataBindY(sensor1_Q);
+        }
 
-        void wi_DataAvailable(object sender, WaveInEventArgs e)
+        string WriteRead(string requiry)
+        {
+            requiry += System.Environment.NewLine;
+            serialPort1.Write(requiry);
+            string message;
+            try
+            {
+                message = serialPort1.ReadLine();
+                return message;
+            }
+            catch (TimeoutException)
+            {
+                return "err-cannot write/read";
+            }
+        }
+
+        bool SearchKeUSB() // maybe too complex, but vrode norm
+        {
+            string[] ports = SerialPort.GetPortNames();
+            for (int i = 0; i < ports.Length; i++)
+            {
+                serialPort1.PortName = ports[i];
+                try
+                {
+                    serialPort1.Open();
+                    if (WriteRead("$KE").Substring(0, 3) == "#OK") break;
+                }
+                catch { }
+                serialPort1.Close();
+            }
+            return serialPort1.IsOpen;
+        }
+
+        void wi_DataAvailable(object sender, WaveInEventArgs e) // 
         {
             for (int i = 0; i < e.BytesRecorded; i += 2)
             {
-                myQ.Enqueue(BitConverter.ToInt16(e.Buffer, i));
-                myQ.Dequeue();
+                mic_Q.Enqueue(BitConverter.ToInt16(e.Buffer, i));
+                mic_Q.Dequeue();
             }
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+            //mic constantly updates the at wi_DataAvailable(), independently from the timer)
+
+            //sensors
+            // gettin data from sensor1
+            WriteRead("$KE,IO,SET,1,0");
+            WriteRead("$KE,WR,1,1");
+            string re = WriteRead("$KE,ADC");
+
             try
             {
-                chart1.Series["Series1"].Points.DataBindY(myQ);
-                //chart1.ResetAutoValues();
+                int sensor1_value = Convert.ToInt32("" + re[5] + re[6] + re[7] + re[8]);
+                sensor1_Q.Enqueue(sensor1_value);
+                sensor1_Q.Dequeue();
             }
-            catch
-            {
-                Console.WriteLine("No bytes recorded");
-            }
+            catch { Console.WriteLine("Fuckin error at int sensor1_value = Convert.ToInt32, ADC Value is: " + re); }
+            draw_charts();
         }
     }
 }
