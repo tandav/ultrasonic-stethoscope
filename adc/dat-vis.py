588d920b66d46efc6b8f8bd411be4eb447330031
tandav <alx1dr@gmail.com>
2017-02-15 09:04:08 +0300

    remove dublicating projects

--- adc/dat-vis.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-myarray = np.fromfile('adc001.dat',dtype=int)
-plt.plot(myarray)
-plt.show()
-print(myarray)
-
-# gcc-4.2 -std=c99 -fopenmp FFT_parallel.c && ./a.out
