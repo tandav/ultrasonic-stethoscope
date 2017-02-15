9f40b2eab7f56fb34afd60914cd43f359efc1cd3
tandav <alx1dr@gmail.com>
2017-02-15 09:06:27 +0300

    add la12-adc MVP

---
+++ Rudshel/forms-timer-label/forms-timer-label/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace forms_timer_label
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(119, 389);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(113, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 31);
+            this.label1.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(645, 389);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(119, 41);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(576, 300);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(820, 465);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "\\";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ Rudshel/forms-timer-label/forms-timer-label/Form1.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using RshCSharpWrapper;
+using RshCSharpWrapper.RshDevice;
+
+namespace forms_timer_label
+{
+    public partial class Form1 : Form
+    {
+        int counter = 0;
+        Queue<double> myQ; //
+        int x_axis_points = 400;
+        //Путь к каталогу, в который будет произведена запись данных.
+        const string FILEPATH = "C:\\Users\\tandav\\Desktop\\data\\";
+
+        //Служебное имя платы, с которой будет работать программа.
+        const string BOARD_NAME = "LAn10_12USB";
+        //Размер собираемого блока данных в отсчётах (на канал).
+        const uint BSIZE = 1048576;
+        //Частота дискретизации. 
+        const double SAMPLE_FREQ = 1.0e+8;
+
+        //Создание экземляра класса для работы с устройствами
+        Device device = new Device();
+
+
+        //Код выполнения операции.
+        RSH_API st;
+
+        // Время ожидания(в миллисекундах) до наступления прерывания. Прерывание произойдет при полном заполнении буфера. 
+        uint waitTime = 100000;
+        uint loopNum = 0;
+
+        //Структура для инициализации параметров работы устройства.  
+        RshInitMemory p = new RshInitMemory();
+
+        uint activeChanNumber = 0, serNum = 0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            // Some Initialisation Work
+
+            myQ = new Queue<double>(Enumerable.Repeat(0.0, x_axis_points).ToList()); // fill myQ w/ zeros
+            //chart1.ChartAreas[0].AxisY.Minimum = -10000;
+            //chart1.ChartAreas[0].AxisY.Maximum = 10000;
+
+
+            //загрузка и подключение к библиотеке абстракции устройства
+            st = device.EstablishDriverConnection(BOARD_NAME);
+            if (st != RSH_API.SUCCESS) SayGoodBye(st);
+
+            Console.WriteLine("\n--> Start-Stop data acquisition mode <--\n\n");
+
+            //=================== ИНФОРМАЦИЯ О ЗАГРУЖЕННОЙ БИБЛИОТЕКЕ ====================== 
+            string libVersion, libname, libCoreVersion, libCoreName;
+
+            st = device.Get(RSH_GET.LIBRARY_VERSION_STR, out libVersion);
+            if (st != RSH_API.SUCCESS) SayGoodBye(st);
+
+            st = device.Get(RSH_GET.CORELIB_VERSION_STR, out libCoreVersion);
+            st = device.Get(RSH_GET.CORELIB_FILENAME, out libCoreName);
+            st = device.Get(RSH_GET.LIBRARY_FILENAME, out libname);
+
+            Console.WriteLine("Library Name: {0:d}", libname);
+            Console.WriteLine("Library Version: {0:d}", libVersion);
+            Console.WriteLine("\nCore Library Name: {0:d}", libCoreName);
+            Console.WriteLine("Core Library Version: {0:d}", libCoreVersion);
+
+            //===================== ПРОВЕРКА СОВМЕСТИМОСТИ =================================  
+
+            uint caps = (uint)RSH_CAPS.SOFT_GATHERING_IS_AVAILABLE;
+            // Проверим, поддерживает ли плата функцию сбора данных в режиме "Старт-Стоп".
+            st = device.Get(RSH_GET.DEVICE_IS_CAPABLE, ref caps);
+            if (st != RSH_API.SUCCESS) SayGoodBye(st);
+
+            //========================== ИНИЦИАЛИЗАЦИЯ =====================================        
+
+            //Подключаемся к устройству. Нумерация начинается с 1.
+            st = device.Connect(1);
+            if (st != RSH_API.SUCCESS) SayGoodBye(st);
+
+            /*
+            Можно подключиться к устройству по заводскому номеру.
+            uint serialNumber = 11111;
+            st = device.Connect(serialNumber, RSH_CONNECT_MODE.SERIAL_NUMBER);
+            if (st != RSH_API.SUCCESS) return SayGoodBye(st);
+            */
+
+
+            //Запуск сбора данных программный. 
+            p.startType = (uint)RshInitMemory.StartTypeBit.Program;
+            //Размер внутреннего блока данных, по готовности которого произойдёт прерывание.
+            p.bufferSize = BSIZE;
+            //Частота дискретизации.
+            p.frequency = SAMPLE_FREQ;
+
+            //Сделаем 0-ой канал активным.
+            p.channels[0].control = (uint)RshChannel.ControlBit.Used;
+            //Зададим коэффициент усиления для 0-го канала.
+            p.channels[0].gain = 1;
+
+            //Инициализация устройства (передача выбранных параметров сбора данных)
+            //После инициализации неправильные значения в структуре будут откорректированы.
+            st = device.Init(p);
+            if (st != RSH_API.SUCCESS) SayGoodBye(st);
+
+            //=================== ИНФОРМАЦИЯ О ПРЕДСТОЯЩЕМ СБОРЕ ДАННЫХ ====================== 
+
+            device.Get(RSH_GET.DEVICE_ACTIVE_CHANNELS_NUMBER, ref activeChanNumber);
+            device.Get(RSH_GET.DEVICE_NAME_VERBOSE, out libname);
+            device.Get(RSH_GET.DEVICE_SERIAL_NUMBER, ref serNum);
+
+            Console.WriteLine(
+                "\nThe name of the connected device: {0} " +
+                "\nSerial number of the connected device: {1:d} " +
+                "\nData to be collected: {2:d} samples " +
+                "\nADC frequency: {3:f} Hz " +
+                "\nThe number of active channels: {4:d} " +
+                "\nThe estimated time of gathering completion: {5:f} seconds",
+                libname, serNum, p.bufferSize, p.frequency, activeChanNumber, (p.bufferSize / p.frequency));
+
+
+            Console.WriteLine("\n=============================================================\n");
+
+
+
+            timer1.Interval = 50;
+            timer1.Start();
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            SayGoodBye(RSH_API.SUCCESS);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            st = device.Start(); // Запускаем плату на сбор буфера.
+            if (st != RSH_API.SUCCESS) SayGoodBye(st);
+
+            Console.WriteLine("\n--> Collecting buffer...\n", BOARD_NAME);
+
+            if ((st = device.Get(RSH_GET.WAIT_BUFFER_READY_EVENT, ref waitTime)) == RSH_API.SUCCESS)    // Ожидаем готовность буфера.
+            {
+                Console.WriteLine("\nInterrupt has taken place!\nWhich means that onboard buffer had filled completely.");
+
+                device.Stop();
+
+                //Буфер с данными в мзр.
+                short[] userBuffer = new short[p.bufferSize * activeChanNumber];
+                //Буфер с данными в вольтах.
+                double[] userBufferD = new double[p.bufferSize * activeChanNumber];
+
+                //Получаем буфер с данными.
+                st = device.GetData(userBuffer);
+                if (st != RSH_API.SUCCESS) SayGoodBye(st);
+
+                //Получаем буфер с данными. В этом буфере будут те же самые данные, но преобразованные в вольты.
+                st = device.GetData(userBufferD);
+                if (st != RSH_API.SUCCESS) SayGoodBye(st);
+
+                // Выведем в консоль данные в вольтах. (первые 10 измерений)
+                for (int i = 0; i < 10; i++)
+                    Console.WriteLine(userBufferD[i].ToString());
+
+
+                for (int i = 0; i < x_axis_points; i++) // TODO: fix that shame. (userbufferD is much bigger that x_axis_points)
+                {
+                    myQ.Enqueue(userBufferD[i]);
+                    myQ.Dequeue();
+                }
+
+                try
+                {
+                    chart1.Series["Series1"].Points.DataBindY(myQ);
+                    //chart1.ResetAutoValues();
+                }
+                catch
+                {
+                    Console.WriteLine("No bytes recorded");
+                }
+
+                try // запись данных в файл
+                {
+                    string filePath = FILEPATH + BOARD_NAME + "_StartStop" + (++loopNum).ToString() + ".dat";
+                    WriteData(userBuffer, filePath);
+                    Console.WriteLine("\nData successfully collected and saved to {0}\nFile size: {1} kilobytes\n", filePath, (userBuffer.Length * sizeof(short)) / 1024);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Exception has happend! " + ex.Message);
+                }
+            }
+            else
+                SayGoodBye(st);
+
+            //label1.Text = counter.ToString();
+            //counter += 1;
+        }
+
+
+
+        static void WriteData(short[] values, string path)
+        {
+            using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write))
+            {
+                using (BinaryWriter bw = new BinaryWriter(fs))
+                {
+                    foreach (short value in values)
+                    {
+                        bw.Write(value);
+                    }
+                }
+            }
+        }
+
+
+        public static int SayGoodBye(RSH_API statusCode)
+        {
+            string errorMessage;
+            Device.RshGetErrorDescription(statusCode, out errorMessage, RSH_LANGUAGE.RUSSIAN);
+            Console.WriteLine("\n" + errorMessage);
+            Console.WriteLine("\n" + statusCode.ToString() + " ( 0x{0:x} ) ", (uint)statusCode);
+            Console.WriteLine("\n\nPress any key to end up the program.");
+            //Console.ReadKey();
+            return (int)statusCode;
+        }
+    }
+}
