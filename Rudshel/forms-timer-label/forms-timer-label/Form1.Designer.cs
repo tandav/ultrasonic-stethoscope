d14782ab5999bdadf4c357121a8caa9b97fdae2a
tandav <alx1dr@gmail.com>
2017-03-13 23:06:41 +0300

    good Start-Stop MVP
    * time logs
    * very hard values reducing, 'cause of huge amount of  raw data
    * rename iBuffer -> buffer
    * rename IBUFCNT -> buffers_in_series

--- Rudshel/forms-timer-label/forms-timer-label/Form1.Designer.cs
+++ Rudshel/forms-timer-label/forms-timer-label/Form1.Designer.cs
@@ -28,14 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
@@ -43,8 +41,6 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
@@ -106,10 +102,6 @@
             this.chart1.TabIndex = 5;
             this.chart1.Text = "chart1";
             // 
-            // timer1
-            // 
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
-            // 
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
@@ -141,9 +133,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(1058, 248);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.Size = new System.Drawing.Size(93, 13);
             this.label1.TabIndex = 8;
-            this.label1.Text = "buffer count:";
+            this.label1.Text = "buffer array count:";
             // 
             // label2
             // 
@@ -157,46 +149,26 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(1058, 274);
+            this.label3.Location = new System.Drawing.Point(1058, 270);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(79, 13);
+            this.label3.Size = new System.Drawing.Size(75, 13);
             this.label3.TabIndex = 10;
-            this.label3.Text = "chart_updated:";
+            this.label3.Text = "buffer array dt:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(1171, 274);
+            this.label4.Location = new System.Drawing.Point(1171, 270);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(35, 13);
             this.label4.TabIndex = 11;
             this.label4.Text = "label4";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(1058, 298);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(86, 13);
-            this.label5.TabIndex = 12;
-            this.label5.Text = "buffer_list.Count:";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(1171, 298);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(35, 13);
-            this.label6.TabIndex = 13;
-            this.label6.Text = "label6";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1223, 543);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -222,7 +194,6 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.NumericUpDown numericUpDown1;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
-        private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
@@ -230,8 +201,6 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
     }
 }
 
--- Rudshel/forms-timer-label/forms-timer-label/Form1.cs
+++ Rudshel/forms-timer-label/forms-timer-label/Form1.cs
@@ -6,18 +6,18 @@ using System.IO;
 using RshCSharpWrapper;
 using RshCSharpWrapper.RshDevice;
 using System.ComponentModel;
+using System.Diagnostics;
 
 namespace forms_timer_label
 {
     public partial class Form1 : Form
     {
-        const string FILEPATH    = "C:\\Users\\tandav\\Desktop\\data\\"; //Путь к каталогу, в который будет произведена запись данных.
-        const string BOARD_NAME  = "LAn10_12USB";                        //Служебное имя платы, с которой будет работать программа.
-        const uint   BSIZE       = 524288;                               // буфер, количество значений, собираемых за раз. Чем реже обращаешься тем лучше (чем больше буффер)
-        const double SAMPLE_FREQ = 8.0e+7;                              //Частота дискретизации. 
-        const uint IBUFCNT       = 5;  //Количество внутренних буферов в конструируемом буфере данных.
-        int x_axis_points        = 500;
-        const int timer_tick_interval = 500;
+        const string FILEPATH       = "C:\\Users\\tandav\\Desktop\\data\\"; //Путь к каталогу, в который будет произведена запись данных.
+        const string BOARD_NAME     = "LAn10_12USB";                        //Служебное имя платы, с которой будет работать программа.
+        const uint   BSIZE          = 524288;                               // буфер, количество значений, собираемых за раз. Чем реже обращаешься тем лучше (чем больше буффер)
+        const double SAMPLE_FREQ    = 8.0e+7;                               //Частота дискретизации. 
+        const int buffers_in_series = 100;                                   //Количество внутренних буферов в конструируемом буфере данных.
+        int x_axis_points           = 200;
 
         double[] values_to_draw;
         Device device = new Device(); //Создание экземляра класса для работы с устройствами
@@ -27,7 +27,13 @@ namespace forms_timer_label
         //List<double> buffer_list = new List<double>();
         int chart_updated_counter = 0;
         bool getting_data;
-        double[] buffer;
+        double[] buffer_array;
+        long series_dt = 0;
+        Stopwatch stopwatch = new Stopwatch();
+        Stopwatch stopwatch2 = new Stopwatch(); // need second stopwatch 'cause they works async
+
+        //Stopwatch stopwatch = Stopwatch.StartNew();
+
 
         public Form1()
         {
@@ -49,8 +55,6 @@ namespace forms_timer_label
         private void button1_Click(object sender, EventArgs e)
         {
             values_to_draw = new double[x_axis_points];
-
-
             getting_data = true;
             backgroundWorker1.RunWorkerAsync();
 
@@ -108,8 +112,6 @@ namespace forms_timer_label
 
             st = device.Connect(1); //Подключаемся к устройству. Нумерация начинается с 1.
             if (st != RSH_API.SUCCESS) SayGoodBye(st);
-
-
            
             p.startType = (uint)RshInitMemory.StartTypeBit.Program; //Запуск сбора данных программный. 
             p.bufferSize = BSIZE; //Размер внутреннего блока данных, по готовности которого произойдёт прерывание.
@@ -123,75 +125,82 @@ namespace forms_timer_label
             if (st != RSH_API.SUCCESS) SayGoodBye(st);
 
 
-
-            Console.WriteLine("\n=============================================================\n");
-
-            //Буфер с "непрерывными" данными (для записи в файл).            
-            buffer = new double[p.bufferSize * IBUFCNT];
-
-            //Получаемый из платы буфер.
-            double[] iBuffer = new double[p.bufferSize];
+                      
+            double[] buffer = new double[p.bufferSize]; //Получаемый из платы буфер.
+            //buffer_array = new double[p.bufferSize * buffers_in_series];
 
 
             //Время ожидания(в миллисекундах) до наступления прерывания. Прерывание произойдет при полном заполнении буфера. 
             uint waitTime = 100000; // default = 100000
             //uint loopNum = 0;
-            int buffer_counter = 0;
+            int buffer_array_counter = 0; // counts the series of buffer arrays
 
             while (getting_data)
             {
+                stopwatch.Restart();
                 st = device.Start(); // Запускаем плату на сбор буфера.
                 if (st != RSH_API.SUCCESS) SayGoodBye(st);
 
-                for (int loop = 0; loop < IBUFCNT; loop++)
+                for (int i = 0, j = 0; i < buffers_in_series; i++) // Series of buffers
                 {
-                    //Console.WriteLine("\n--> Collecting buffer...\n", BOARD_NAME);
-                    //Console.WriteLine("now before WAIT_BUFFER_READY_EVENT, loop = {0}", loop);
-
                     st = device.Get(RSH_GET.WAIT_BUFFER_READY_EVENT, ref waitTime);
                     if (st != RSH_API.SUCCESS) SayGoodBye(st);
 
-                    //Console.WriteLine("succes at wait, loop = {0}", loop);
-
-
-                    //Получаем буфер с данными. В этом буфере будут те же самые данные, но преобразованные в вольты.
-                    st = device.GetData(iBuffer);
+                    st = device.GetData(buffer); // very big amount of data
                     if (st != RSH_API.SUCCESS) SayGoodBye(st);
-                    device.Stop(); // probably should del this (for full persistance)
+                    device.Stop();
 
-
-                    // Скопируем данные в "непрерывный" буфер.
-                    iBuffer.CopyTo(buffer, loop * iBuffer.Length);
-
-
-                    //buffer_list.AddRange(buffer);
-                    //buffers_storage.AddRange(buffer);
+                    values_to_draw[i] = buffer.Average();
+                    //if (i  == 0 && j < x_axis_points)
+                    //{
+                    //    values_to_draw[j++] = buffer[0];
+                    //}
+                    //reduce(buffer, x_axis_points / buffers_in_series).CopyTo(values_to_draw, i * x_axis_points / buffers_in_series); // reducing that shit
+                    //buffer.CopyTo(buffer_array, i * buffer.Length);
                 }
 
-                for (int i = 0, j = 0; i < buffer.Length; i++) // skip some values
-                {
-                    if (i % buffer.Length / x_axis_points == 0)
-                    {
-                        values_to_draw[j++] = buffer[i];
-                    }
-                    buffer[i] = 0; // cleaning
-                }
-
-                buffer_counter++;
-                backgroundWorker1.ReportProgress(buffer_counter);
-
+                //for (int i = 0, j = 0; i < buffer_array.Length; i++) // skip some values
+                //{
+                //    if (i % buffer.Length / x_axis_points == 0 && j < values_to_draw.Length)
+                //    {
+                //        values_to_draw[j++] = buffer_array[i];
+                //    }
+                //    buffer_array[i] = 0; // cleaning
+                //}
+                buffer_array_counter++;
+                stopwatch.Stop();
+                series_dt = stopwatch.ElapsedMilliseconds;
+                backgroundWorker1.ReportProgress(buffer_array_counter);
             }
-
-
         }
 
         private void backgroundWorker1_ProgressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
         {
-            chart1.Series["Series1"].Points.DataBindY(values_to_draw);
-            chart_updated_counter++;
-            label4.Text = chart_updated_counter.ToString();
-            // try update chart here
+            stopwatch2.Restart();
+            Console.Write("Get-data time:\t" + series_dt + "ms\t\t");
+            chart1.Series[0].Points.DataBindY(values_to_draw);
             label2.Text = e.ProgressPercentage.ToString();
+            stopwatch2.Stop();
+            Console.Write("Redraw and log time:\t" + stopwatch.ElapsedMilliseconds + "ms\n");
+
+
+            // try update chart here
+            //label4.Text = stopwatch.ElapsedMilliseconds.ToString();
+            //label4.Text = stopwatch.ElapsedMilliseconds.ToString();
+
+        }
+
+        private double[] reduce(double[] array, int array_out_length)
+        {
+            double[] array_out = new double[array_out_length];
+            for (int i = 0, j = 0; i < array.Length; i++)
+            {
+                if (i % array.Length / array_out_length == 0)
+                {
+                    array_out[j++] = array[i];
+                }
+            }
+            return array_out;
         }
 
         private void numericUpDown1_ValueChanged(object sender, EventArgs e)
