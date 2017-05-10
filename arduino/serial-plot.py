ac41e0baa46c2d342dae33e0f86306a754b542c7
tnd <alx1dr@gmail.com>
2017-05-10 12:21:43 +0300

    multiprocessing mvp (isn't work)

--- arduino/serial-plot.py
+++ arduino/serial-plot.py
@@ -1,16 +1,52 @@
-import serial
+from multiprocessing import Process
 import numpy as np
+import serial
 import matplotlib
 matplotlib.use('TKAgg')
 from matplotlib import pyplot as plt
 from matplotlib import animation
-import serial
+
+
+
+buffer = []
+
 
 #init serial device
 try:
     ser = serial.Serial('/dev/cu.usbmodem1421', 250000)
 except Exception as e:
     print("Can't connect to serial port")
+
+
+def gather_data():
+    global buffer
+    while True:
+        try:
+            # bytesToRead = ser.inWaiting()
+            # raw_bytes = ser.read(bytesToRead)
+            raw_bytes = ser.read_all()
+            
+            if len(raw_bytes) > 1:
+                # print(raw_bytes)
+                bytes_array = raw_bytes.split(b'\r\n')
+                for b in bytes_array:
+                    try:
+                        buffer.append(float(b))
+                    except Exception as e:
+                        pass
+                        # print("float conversion error:", e)
+                print(len(buffer))
+                print("----------------")
+        except Exception as e:
+            # pass
+            # print(e)
+            print("Serial readline() error:", e)
+
+
+
+p1 = Process(target=gather_data)
+p1.start()
+p1.join()
 
 x_axis_points = 1000
 y = np.zeros(x_axis_points)
@@ -21,6 +57,10 @@ ax = plt.axes(xlim=(0, 2), ylim=(-3, 3))
 line, = ax.plot(np.linspace(0, 2, x_axis_points), y, lw=2)
 
 
+
+
+
+
 # # initialization function: plot the background of each frame
 # def init():
 #     line.set_data(np.linspace(0, 2, 10000), np.linspace(0, 2, 10000))
@@ -29,12 +69,14 @@ line, = ax.plot(np.linspace(0, 2, x_axis_points), y, lw=2)
 # animation function.  This is called sequentially
 def animate(i):
     global y
+    global buffer
     x = np.linspace(0, 2, x_axis_points)
-    try:
-        y = np.append(y, float(ser.readline()))
-        y = np.delete(y, 0)
-    except Exception as e:
-        print("Serial readline() error")
+    # y = np.sin(2 * np.pi * (x - 0.01 * i))
+    y = y[len(buffer):]
+    # y.extend(buffer)
+    np.append(y, buffer)
+    buffer = []
+
     line.set_data(x, y)
     return line,
 
