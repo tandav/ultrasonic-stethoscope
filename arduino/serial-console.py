6be1b5d94f9d75e9fbe898479f69a7a222af39ac
tnd <alx1dr@gmail.com>
2017-05-10 10:51:28 +0300

    some fix

--- arduino/serial-console.py
+++ arduino/serial-console.py
@@ -7,13 +7,18 @@ from matplotlib import animation
 import serial
 
 #init serial device
-ser = serial.Serial('/dev/cu.usbmodem1421', 250000)
-y = np.zeros(1000)
+try:
+    ser = serial.Serial('/dev/cu.usbmodem1421', 250000)
+except Exception as e:
+    print("Can't connect to serial port")
+
+x_axis_points = 1000
+y = np.zeros(x_axis_points)
 
 # First set up the figure, the axis, and the plot element we want to animate
 fig = plt.figure()
 ax = plt.axes(xlim=(0, 2), ylim=(-3, 3))
-line, = ax.plot(np.linspace(0, 2, 1000), y, lw=2)
+line, = ax.plot(np.linspace(0, 2, x_axis_points), y, lw=2)
 
 
 # # initialization function: plot the background of each frame
@@ -24,21 +29,17 @@ line, = ax.plot(np.linspace(0, 2, 1000), y, lw=2)
 # animation function.  This is called sequentially
 def animate(i):
     global y
-    x = np.linspace(0, 2, 1000)
+    x = np.linspace(0, 2, x_axis_points)
     try:
         y = np.append(y, float(ser.readline()))
         y = np.delete(y, 0)
     except Exception as e:
-        pass
+        print("Serial readline() error")
     line.set_data(x, y)
     return line,
 
-
-def data_gen():
-    while True:
-        yield np.random.rand(10)
 # call the animator.  blit=True means only re-draw the parts that have changed.
-anim = animation.FuncAnimation(fig, animate, frames=200, interval=1, blit=True)
+anim = animation.FuncAnimation(fig, animate, frames=200, interval=20, blit=True)
 # anim = animation.FuncAnimation(fig, animate, init_func=init, frames=200, interval=20, blit=True)
 
 plt.show()
