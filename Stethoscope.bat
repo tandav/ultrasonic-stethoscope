f4f498412c0113930d23fda7c89f4122d5b817ae
tnd <alx1dr@gmail.com>
2017-11-22 15:50:38 +0300

    installators

--- Stethoscope.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-cmd /k "E:\Soft\Anaconda\Scripts\activate.bat & python app.py"
---
+++ run.bat
@@ -0,0 +1,4 @@
+call C:\Users\Anastasia\Miniconda3\Scripts\activate.bat
+call activate .\uv
+python app.py rem if fail: reinstall
+pause
