8bb532c11bac4ccc08b590aa7d1a26fd698723e9
tnd <alx1dr@gmail.com>
2018-04-15 21:03:05 +0300

    [add] test for cell neighbours of distance=2

--- lungs-model/flat-array/for-flat-test.py
+++ lungs-model/flat-array/for-flat-test.py
@@ -12,10 +12,9 @@ N = 16
 # S = 10 # number of slices / CT-images / scans
 S = 16 # first test simple case when N == S
 
-# r = np.random.random((S, N, N))
-r = np.load('r.npy')
+r = np.random.random((S, N, N))
+# r = np.load('r.npy')
 ro  = 1e-5 + 1.24e-3*r - 2.83e-7*r*r + 2.79e-11*r*r*r
-# c = np.empty_like(r)
 c = (ro + 0.112) * 1.38e-6
 
 # тут не факт что все 1
@@ -31,8 +30,10 @@ K2_3_ro = K2 / 3 / ro
 # P_pp = np.random.random((S, N, N)) # previous previous t - 2
 # P_p  = np.random.random((S, N, N)) # previous          t - 1
 
-P_pp = np.load('P_pp.npy')
-P_p = np.load('P_p.npy')
+P_pp = np.arange(N**3).reshape(N, N, N) * 10.
+P_p = np.arange(N**3).reshape(N, N, N) * 12.
+# P_pp = np.load('P_pp.npy')
+# P_p = np.load('P_p.npy')
 # P    = np.zeros((S, N, N), dtype=np.float64) # current           t
 
 # np.save('P_pp.npy', P_pp)
@@ -106,14 +107,14 @@ def old_slow1(P_pp, P_p):
                 )
                 P[i, j, k] += 4/3 * K2[i, j, k] * s1
 
-                # s2 = (P_p[i + 2, j    , k    ] + \
-                #       P_p[i - 2, j    , k    ] + \
-                #       P_p[i    , j + 2, k    ] + \
-                #       P_p[i    , j - 2, k    ] + \
-                #       P_p[i    , j    , k + 2] + \
-                #       P_p[i    , j    , k - 2]
-                # )
-                # P[i, j, k] -= K2[i, j, k] / 12 * s2
+                s2 = (P_p[i + 2, j    , k    ] + \
+                      P_p[i - 2, j    , k    ] + \
+                      P_p[i    , j + 2, k    ] + \
+                      P_p[i    , j - 2, k    ] + \
+                      P_p[i    , j    , k + 2] + \
+                      P_p[i    , j    , k - 2]
+                )
+                P[i, j, k] -= K2[i, j, k] / 12 * s2
 
                 # s3 = ((P_p[i + 1, j    , k    ] - P_p[i-1 , j  , k  ]) - (P_p[i + 2, j, k] - P_p[i - 2, j, k]) / 8) * (ro[i + 1, j, k] - ro[i - 1, j, k])
                 # s4 = ((P_p[i    , j + 1, k    ] - P_p[i   , j-1, k  ]) - (P_p[i, j + 2, k] - P_p[i, j - 2, k]) / 8) * (ro[i, j + 1, k] - ro[i, j - 1, k])
@@ -135,11 +136,21 @@ def old_slow2(P_pp, P_p):
     
     # P_p_flat = P_p.flatten()
 
-    cell_indeces_flat = np.arange(N**3).reshape(N, N, N)[1:-1, 1:-1, 1:-1].flatten().reshape(-1, 1) # vertical vector
+    cell_indeces_flat = np.arange(N**3).reshape(N, N, N)[2:-2, 2:-2, 2:-2].flatten().reshape(-1, 1) # vertical vector
+
     neighbours1_indeces_flat = cell_indeces_flat + np.array([-1, 1, -N, N, -N**2, N**2])      # i±1 j±1 k±1 
-    neighbours_cell_all = P_p.flatten()[neighbours1_indeces_flat] # each row contains 6 neighbors of cell 
-    neighbours_cell_all_sum = np.sum(neighbours_cell_all, axis=1)
-    P[1:-1, 1:-1, 1:-1] += 4/3 * K2[1:-1, 1:-1, 1:-1] * (P.flatten()[cell_indeces_flat.reshape(-1)] + neighbours_cell_all_sum).reshape(N-2, N-2, N-2)
+    neighbours2_indeces_flat = cell_indeces_flat + np.array([-1, 1, -N, N, -N**2, N**2]) * 2  # i±2 j±2 k±2 
+
+    neighbours1_cell_all = P_p.flatten()[neighbours1_indeces_flat] # each row contains 6 neighbors of cell 
+    neighbours2_cell_all = P_p.flatten()[neighbours1_indeces_flat] # each row contains 6 neighbors of cell 
+
+    neighbours1_cell_all_sum = np.sum(neighbours1_cell_all, axis=1)
+    neighbours2_cell_all_sum = np.sum(neighbours2_cell_all, axis=1)
+
+
+
+    P[2:-2, 2:-2, 2:-2] += 4/3 * K2[2:-2, 2:-2, 2:-2] * (P.flatten()[cell_indeces_flat.reshape(-1)] + neighbours1_cell_all_sum).reshape(N-4, N-4, N-4)
+    P[2:-2, 2:-2, 2:-2] -= K2[2:-2, 2:-2, 2:-2] /  12 * (P.flatten()[cell_indeces_flat.reshape(-1)] + neighbours2_cell_all_sum).reshape(N-4, N-4, N-4)
 
 
     # cell_indeces_flat = np.arange(N**3).reshape(N, N, N)[2:-2, 2:-2, 2:-2].flatten().reshape(-1, 1) # vertical vector
@@ -204,3 +215,5 @@ print(P1[0,0,0])
 print(P2[0,0,0])
 
 print(f'delta = {abs(P1[0,0,0] - P2[0,0,0])}')
+
+print(np.sum(P1) - np.sum(P2))
